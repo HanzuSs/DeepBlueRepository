9ac45d072b410521fc38d6102dc58d753ff71471
Samuel Almanza <almanzasamuel2006@gmail.com>
2026-09-07 11:36:47 -0500

    Paso 28

--- deepblue-rescue/src/main/java/com/deepblue/rescue/domain/Animal.java
+++ deepblue-rescue/src/main/java/com/deepblue/rescue/domain/Animal.java
@@ -64,6 +64,12 @@ public class Animal {
             medicalRecord.setAnimal(this);
         }
     }
+    @OneToMany(mappedBy = "animal")
+    private List<Treatment> treatments = new ArrayList<>();
+
+    public List<Treatment> getTreatments() {
+        return treatments;
+    }
 
     public Long getId() {
         return id;
